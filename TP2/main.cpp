aa671ac2547879ac5ea78e61b6ba76a547976a69
Etienne Dumai <etienne.dumai@gmail.com>
2024-09-16 09:32:33 +0200

    Fin de la Premiere question

--- TP2/main.cpp
+++ TP2/main.cpp
@@ -5,6 +5,13 @@ using namespace std;
 
 
 int main(){
-    
+    Voiture voit1("RenaultClio", "123AB64");
+    Voiture voit2("Peugeot106", "678CD96");
+    Voiture voit3("CitroenPicasso", "456EF75");
+    Individu ind1("Dupont", "Pierre");
+    Individu ind2("Martin", "Louis");
+    Individu ind3("Durand", "Marcel");
+    cout << voit1.toString() << endl << voit2.toString() << endl << voit3.toString() << endl;
+    cout << ind1.toString() << endl << ind2.toString() << endl << ind3.toString() << endl;
     return 0;
 }

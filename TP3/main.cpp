bcd132545b9aa80cdd7bce0d42b2890dc452165a
Etienne Dumai <etienne.dumai@gmail.com>
2024-09-24 15:07:46 +0200

    Fin de la question 2 partie f (derniere)

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 template <class T, class A = allocator<T>>
 class List
 {
-    
+
     typedef T value_type;
     typedef value_type *iterator;
     List();
@@ -23,4 +23,11 @@ void List<T, A>::listeSimple()
     uneListeS.push_back(ListS::value_type("Yann"));
     uneListeS.push_back(ListS::value_type("Philippe"));
     uneListeS.push_back(ListS::value_type("Patrick"));
+    ListeS ::iterator iteratorListeS;
+    iteratorListeS = uneListeS.begin();
+    while (iteratorListeS != uneListeS.end())
+    {
+        cout << *iterateurListeS << endl;
+        iterateurListeS++;
+    }
 }

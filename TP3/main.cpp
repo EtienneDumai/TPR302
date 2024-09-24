c6c0a24314bb138e418311b65e3fd02143ac9311
Etienne Dumai <etienne.dumai@gmail.com>
2024-09-24 14:49:30 +0200

    Question 2 partie C finie

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -4,11 +4,23 @@ using namespace std;
 template <class T, class A = allocator<T>>
 class List
 {
-    public:
+    
     typedef T value_type;
     typedef value_type *iterator;
+    List();
     void push_back(const value_type &val);
     iterator begin();
     iterator end();
+    void listeSimple();
 };
 
+template <class T, class A>
+void List<T, A>::listeSimple()
+{
+    typedef list<string> ListS;
+    ListS uneListeS;
+    uneListeS.push_back(ListS::value_type("Pantxika"));
+    uneListeS.push_back(ListS::value_type("Yann"));
+    uneListeS.push_back(ListS::value_type("Philippe"));
+    uneListeS.push_back(ListS::value_type("Patrick"));
+}
